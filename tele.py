fc1e3fbd65ec771c41368c77783c58abbd8299af
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-04-14 04:56:24 +0500

    Update tele.py

--- tele.py
+++ tele.py
@@ -8,15 +8,7 @@ client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
 from telethon.tl.types import MessageEntityCustomEmoji
 
 def get_premium_emojis(message):
-    if not message.entities:
-        return []
-
-    premium_emojis = []
-    for entity in message.entities:
-        if isinstance(entity, MessageEntityCustomEmoji):
-            premium_emojis.append(entity)
-    
-    return premium_emojis
+    return message.entities or []
 
 def is_original_post(event):
     """Check if the message is an original post (not forwarded)"""
