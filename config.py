82638b34105a0121a6c82ebf05d7958d60968221
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-04-14 08:10:28 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
 from telethon.tl.types import MessageEntityCustomEmoji
+import logging
 
+# Loggingni sozlash
+logging.basicConfig(level=logging.INFO, filename="bot_log.txt", 
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+
+def log_error(message):
+    logging.error(message)
+
+def log_info(message):
+    logging.info(message)
+
+# API sozlamalari
 API_ID = 29595868
 API_HASH = 'a09a969ce2b4e13726812ab8e696cd18'
 SESSION_NAME = 'my_bot'
 
+# Adminlar
 admin = [619839487, 1918760732]
 
+# Telegram kanal identifikatorlari
 ALL_ID = ["nodavlattalim", "abitur24", "Talim_Live", "Talim24uz", "ai_lingoBot", "TRANSLATE_TRANSLATOR_PEREVODCHIK"]
 
+# Kanalga qo'shilgan matnlar
 ALL_TEXT = [
     "🇺🇿 @nodavlattalim — nodavlat oliy ta’lim muassasalari haqida rasmiy xabarlar!",
     "Safimizga qo'shiling👇\nhttps://t.me/+Xa6LRjERxwo4Njdi",
@@ -24,15 +39,13 @@ CUSTOM_EMOJI_MAP = {
     4: (2, 5350384878254826109),  # ✅
 }
 
-
 def entities_right(original_text: str, num: int):
     if num not in CUSTOM_EMOJI_MAP:
         return []
 
     emoji_length, emoji_id = CUSTOM_EMOJI_MAP[num]
 
-    # new_text = original_text + '\n\n' + ALL_TEXT[num]
-    # Demak, offset — bu original_text uzunligi + 2 (yangi qatordan)
+    # offset — bu original_text uzunligi + 2 (yangi qatordan)
     offset = len(original_text) + 2
 
     return [MessageEntityCustomEmoji(
